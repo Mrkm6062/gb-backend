b625dbf920d0c2edb7740dee409db4b6e0288d2b
Mrkm6062 <akm222143@gmail.com>
2026-03-23 10:20:46 +0530

    all files created

---
+++ src/modules/auth/auth.dto.ts
@@ -0,0 +1,30 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class RegisterDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6, { message: 'Password must be at least 6 characters long' })
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  shopName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  category: string;
+}
+
+export class LoginDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
